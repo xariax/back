fa96813aff2d3ea5cb83a45c0c56926b3d0930b3
Xariax <wojcik.r@onet.pl>
2025-08-08 13:57:55 +0200

    zmiany w cookies

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,15 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
+  // Jeśli zapytanie jest do /logout, pomijamy weryfikację tokena
+  if (req.path === '/logout') {
+    return next();
+  }
+
   const token = req.cookies?.authToken;
-  if (!token) return res.status(401).json({ message: 'Brak tokena autoryzacyjnego' });
+  if (!token) {
+    return res.status(401).json({ message: 'Brak tokena autoryzacyjnego' });
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Nieprawidłowy lub wygasły token' });
+    if (err) {
+      return res.status(403).json({ message: 'Nieprawidłowy lub wygasły token' });
+    }
     req.user = user;
     next();
   });
 }
-
 
 module.exports = authenticateToken;
